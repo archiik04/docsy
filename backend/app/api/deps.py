41e5818988d871f38466ba412241a3946bb3d10f
Archi Kanungo <archi.kanungo2004@gmail.com>
2026-05-22 10:21:42 +0530

    implement /me endpoint for authorization and protect routes

---
+++ backend/app/api/deps.py
@@ -0,0 +1,60 @@
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+from jose import JWTError
+from jose import jwt
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.database import get_db
+from app.core.security import SECRET_KEY
+from app.core.security import ALGORITHM
+
+from app.models.user import User
+
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/api/v1/auth/login"
+)
+
+
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: AsyncSession = Depends(get_db)
+):
+
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials"
+    )
+
+    try:
+
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise credentials_exception
+
+    except JWTError:
+        raise credentials_exception
+
+    query = select(User).where(
+        User.id == user_id
+    )
+
+    result = await db.execute(query)
+
+    user = result.scalar_one_or_none()
+
+    if user is None:
+        raise credentials_exception
+
+    return user
--- backend/app/api/v1/routes/auth.py
+++ backend/app/api/v1/routes/auth.py
@@ -15,7 +15,7 @@ from app.schemas.auth import TokenResponse
 
 from app.core.security import verify_password
 from app.core.security import create_access_token
-
+from app.api.deps import get_current_user
 
 router = APIRouter()
 
@@ -102,4 +102,15 @@ async def login_user(
     return {
         "access_token": access_token,
         "token_type": "bearer"
+    }
+
+@router.get("/me")
+async def get_me(
+    current_user: User = Depends(get_current_user)
+):
+
+    return {
+        "id": str(current_user.id),
+        "email": current_user.email,
+        "full_name": current_user.full_name
     }
